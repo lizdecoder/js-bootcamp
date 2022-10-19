da70243eca32b8bb193c43f1d11a4f19d9142354
lizdecoder <eys.1908@gmail.com>
2022-10-19 10:12:48 -0400

    created grid elements for maze

--- maze/index.js
+++ maze/index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -31,6 +32,42 @@ const walls = [
 ];
 World.add(world, walls);
     
+// Maze generation
 
+// one attempt for grid
+// const grid = [];
+// for (let i = 0; i < 3; i++) {
+//     grid.push([]);
+//     for (let j = 0; j < 3; j++) {
+//         grid[i].push(false);
+//     }
+// }
 
+// better approach to grid
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
 
+const verticals = Array(cells).fill(null).map(() => Array(cells-1).fill(false));
+const horizontals = Array(cells-1).fill(null).map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+    // If I have visited the cell at [row, column], then return
+
+    // Mark this cell as being visited
+
+    // Assemble randomly-ordered list of neightbors
+
+    // for each neightbor...
+
+    // see if that neightbor is out of bounds
+
+    // if we have visited that neightbor, continue to next neightbor
+
+    // remove a wall from either the horizontals or verticals array
+    
+    // visit that next cell
+};
+
+stepThroughCell(startRow, startColumn);

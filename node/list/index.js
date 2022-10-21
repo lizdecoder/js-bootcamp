7605d540cbea2afe841439d00c4d68f95922abe3
lizdecoder <eys.1908@gmail.com>
2022-10-21 16:15:28 -0400

    update to index.js for readdir func

---
+++ node/list/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+fs.readdir('.', (err, filenames) => {
+    // either err === an error object, or err === null everything is okay
+    if (err) {
+        // error handling code here
+        console.log(err); //not ideal because other code will run
+        // console log requires return to not run error
+        // throw new Error(err); more ideal
+    }
+    console.log(filenames);
+});

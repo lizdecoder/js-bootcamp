bbaeaf252d4ac956650da6c7998cedb72ffef82d
lizdecoder <eys.1908@gmail.com>
2022-10-24 12:04:45 -0400

    index.js implementing async & promises

--- node/list/index.js
+++ node/list/index.js
@@ -3,9 +3,16 @@
 //fs = file system
 const fs = require('fs');
 
+// method #2 lstat with promise
+// const util = require('util');
+// const lstat = util.promisify(fs.lstat);
+
+// method #3 BEST - uses promise-based function
+const { lstat } = fs.promises;
+
 // using process.cwd for current work directory instead of "." to be
 // compatible with other OS
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
     // either err === an error object, or err === null everything is okay
     if (err) {
         // error handling code here
@@ -13,14 +20,59 @@ fs.readdir(process.cwd(), (err, filenames) => {
         // console log requires return to not run error
         // throw new Error(err); more ideal
     }
-    // BAD CODE HERE for learning purposes
+    // BAD CODE HERE for learning purposes - does not keep order
+    // for (let filename of filenames) {
+    //     fs.lstat(filename, (err, stats) => {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //         console.log(filename, stats.isFile());
+    //     });
+    // }
+    // // BAD CODE COMPLETE
+
+    // GOOD CODE HERE: Option #1 - does not allow for more complexities
+    // const allStats = Array(filenames.length).fill(null);
+    
+    // for (let filename of filenames) {
+    //     const index = filenames.indexOf(filename);
+        
+    //     fs.lstat(filename, (err, stats) => {
+    //         if (err) {
+    //             console.log(err);
+    //         }
+    //         allStats[index] = stats;
+    //         // every() returns boolean; if full still have null value
+    //        const ready = allStats.every((stats)=> {
+    //             return stats;
+    //         });
+    //         if (ready) {
+    //             allStats.forEach((stats, index) => {
+    //             console.log(filenames[index], stats.isFile());        
+    //             }); 
+    //         }
+    //     });
+    // }
+
+    // GOOD CODE HERE: Option #2 with async & promise
     for (let filename of filenames) {
-        fs.lstat(filename, (err, stats) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log(filename, stats.isFile());
-        });
+        try {
+        const stats = await lstat(filename);
+        console.log(filename, stats.isFile());
+        } catch (err) {
+            console.log(err);
+        }
     }
-    // BAD CODE COMPLETE
 });
+
+// wrapping lstat in promise method #1
+// const lstat = (filename) => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//             if (err) {
+//                 reject(err);
+//             }
+//             resolve(stats);
+//         });
+//     });
+// };

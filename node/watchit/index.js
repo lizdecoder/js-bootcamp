1e45367257eca9be212eeb17365b585af0beff40
lizdecoder <eys.1908@gmail.com>
2022-10-26 08:46:17 -0400

    updating to use lodash.debounce

--- node/watchit/index.js
+++ node/watchit/index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log("I was executed!");
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+
+const start = debounce(() => {
+    console.log('STARTING USERS PROGRAM');
+}, 100);
+
+chokidar.watch('.')
+    .on('add', start)
+    .on('change', () => console.log('FILE CHANGED'))
+    .on('unlink', () => console.log("FILE UNLINKED"));

8c28b071ebd4eb0ea4044939d2e1640756311e3f
lizdecoder <eys.1908@gmail.com>
2022-10-27 17:27:50 -0400

    index.js displaying simple HTML

--- node/ecomm/index.js
+++ node/ecomm/index.js
@@ -4,7 +4,16 @@ const app = express();
 
 // route handler: what app should do when it receives a network request
 app.get('/', (req, res) => {
-    res.send('hi there!');
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
 });
 
 // app to start listening to network requests on specific port; i.e. 3000

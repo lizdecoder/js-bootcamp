36f38a447f973eaf1753fbdc9b1c7f1726b0d0c1
lizdecoder <eys.1908@gmail.com>
2022-10-27 16:59:38 -0400

    index.js setting up server

--- node/ecomm/index.js
+++ node/ecomm/index.js
@@ -1,1 +1,14 @@
-console.log('hi there.')
+const express = require('express');
+
+const app = express();
+
+// route handler: what app should do when it receives a network request
+app.get('/', (req, res) => {
+    res.send('hi there!');
+});
+
+// app to start listening to network requests on specific port; i.e. 3000
+// if receive error means another process is running on port [3000]
+app.listen(3000, () => {
+    console.log('Listening');
+});

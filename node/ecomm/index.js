49a4d23a44cc7b4d3e6f352a30acf5362a013d1f
lizdecoder <eys.1908@gmail.com>
2022-10-28 15:02:24 -0400

    updating to incl app.use for all route handlers

--- node/ecomm/index.js
+++ node/ecomm/index.js
@@ -1,7 +1,11 @@
 const { application } = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// applies middleware function to all route handlers
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // route handler: what app should do when it receives a network request
 app.get('/', (req, res) => {
@@ -17,29 +21,33 @@ app.get('/', (req, res) => {
     `);
 });
 
+// Attempt #2 to parse data
 // Middleware helper function to parse data from form
 // next is a callback function that comes from express app
-const bodyParser = (req, res, next) => {
-    if (req.method === 'POST') {
-        req.on('data', data => {
-            const parsed = data.toString('utf8').split('&');
-            const formData = {};
-            for (let pair of parsed) {
-                const [key, value] = pair.split('=');
-                formData[key] = value;
-            }
-            req.body = formData;
-            next();
-        });
-    } else {
-        next();
-    }
-};
+// const bodyParser = (req, res, next) => {
+//     if (req.method === 'POST') {
+//         req.on('data', data => {
+//             const parsed = data.toString('utf8').split('&');
+//             const formData = {};
+//             for (let pair of parsed) {
+//                 const [key, value] = pair.split('=');
+//                 formData[key] = value;
+//             }
+//             req.body = formData;
+//             next();
+//         });
+//     } else {
+//         next();
+//     }
+// };
 
+// Attempt #3 to parse data
+// uses outside library for middleware function bodyParser.urlencoded()
 // route handlers
-app.post('/', bodyParser, (req, res) => {
+app.post('/', (req, res) => {
     //get access to email, password, passwordConfirmation
     // req.on is equal to addEventListener
+    // Attempt #1 to parse data
     // manual code to parse data from form
     // req.on('data', data => {
         // const parsed = data.toString('utf8').split('&');
@@ -51,7 +59,6 @@ app.post('/', bodyParser, (req, res) => {
         // console.log(formData);
 
     console.log(req.body);
-    
     res.send('Account created!');
 });
 

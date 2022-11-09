20d6fac078fc5d5228b0ba1773acaeea02735ba3
lizdecoder <eys.1908@gmail.com>
2022-11-08 19:47:02 -0500

    error handler for new prod submission form

--- node/ecomm/routes/admin/products.js
+++ node/ecomm/routes/admin/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
 const productsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
+const {requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 
@@ -10,10 +13,15 @@ router.get('/admin/products', (req, res) => {
 
 // route to show a form to allow admin to create a new product
 router.get('/admin/products/new', (req, res) => {
-
+    res.send(productsNewTemplate({}));
 });
 
 // route to submit form
+router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors)
+    res.send('submitted');
+});
 // route to allow admin to edit
 // route submitting editing form
 // route deletion of product
